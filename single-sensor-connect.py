b6e3f5ddfec08c2d2bc294c0eba6ab84c0462278
Jordan Ashley <jashley6@vols.utk.edu>
2025-06-05 14:27:50 -0700

    :sparkles: Continuous data stream

--- single-sensor-connect.py
+++ single-sensor-connect.py
@@ -3,53 +3,52 @@ import asyncio # prevents BLE bottlenecking
 import argparse # allows CLI argument passing
 from bleak import BleakClient # allows BLE connection
 
-# --- PARSE ADDRESS ARGUMENT --- #
-parser = argparse.ArgumentParser(
-    description="Connect to SHT via MAC address"
-)
-parser.add_argument(
-    '--address', 
-    type=str, 
-    required=True, 
-    help='MAC address of the BLE device (e.g., AA:BB:CC:DD:EE:FF)'
-)
-args    = parser.parse_args()
-address = args.address
+TMP_CHAR_UUID = "00002235-b38d-4985-720e-0f993a68ee41"
+HUM_CHAR_UUID = "00001235-b38d-4985-720e-0f993a68ee41"
 
-# --- CONNECT TO SENSOR --- #
+# --- DECODE SENSOR DATA STREAM --- #
+def parse_sensor_data(traw, hraw):
+    # Data is in bytearray format, '<f' = little-endian float
+    t = struct.unpack('<f', traw)[0]
+    h = struct.unpack('<f', hraw)[0]
+    return t, h
+
+# --- CONNECT TO SENSOR & READ DATA STREAM --- #
 async def main(a):
     print(f"Connecting to {a}")
     async with BleakClient(a, timeout=90.0) as client:
         services = await client.get_services()
+    
+    print(f"Reading data stream from {a}")
+    print("Temperature\tRelative Humidity")
 
-asyncio.run(main(address))
+    while True:
+        try:
+            async with BleakClient(address, timeout=90.0) as client:
+                tmp_raw = await client.read_gatt_char(TMP_CHAR_UUID)
+                hum_raw = await client.read_gatt_char(HUM_CHAR_UUID)
+                temperature, humidity = parse_sensor_data(tmp_raw, hum_raw)
+                print(f"{temperature}\t{humidity}")
 
-LOG_SRVC_UUID = "0000f234-b38d-4985-720e-0f993a68ee41"
-LOG_CHAR_UUID = "0000f238-b38d-4985-720e-0f993a68ee41"
-TMP_CHAR_UUID = "00002235-b38d-4985-720e-0f993a68ee41"
-HUM_CHAR_UUID = "00001235-b38d-4985-720e-0f993a68ee41"
+        except Exception as e:
+            print(f"Failed for {a}: {e}")
+        await asyncio.sleep(1)
 
-# --- DECODE DATA STREAM --- #
-tmp_raw = bytearray(b'R\xb8\xb0A')
-hum_raw = bytearray(b'\x9a\x99;B')
+# --- OUTPUT DATA --- #
 
-
-# --- READ DATA STREAM --- #
-async def read_sensor_data():
-    try:
-        async with BleakClient(address, timeout=90.0) as client:
-            print(f"Reading data stream from {address}")
-            
-            tmp_raw = await client.read_gatt_char(TMP_CHAR_UUID)
-            hum_raw = await client.read_gatt_char(HUM_CHAR_UUID)
-            
-            # Data is in bytearray format, '<f' = little-endian float
-            temperature = struct.unpack('<f', tmp_raw)[0]
-            humidity    = struct.unpack('<f', hum_raw)[0]
-            print("Temperature\tRelative Humidity")
-            print(f"{temperature}\t{humidity}")
-
-    except Exception as e:
-        print(f"Failed for {address}: {e}")
-
-asyncio.run(read_sensor_data())
+if __name__ == "__main__":
+    # parse address argument
+    parser = argparse.ArgumentParser(
+        description="Connect to SHT via MAC address"
+    )
+    parser.add_argument(
+        '--address', 
+        type=str, 
+        required=True, 
+        help='MAC address of the BLE device (e.g., AA:BB:CC:DD:EE:FF)'
+    )
+    args    = parser.parse_args()
+    address = args.address
+    
+    # do the things
+    asyncio.run(main(address))
