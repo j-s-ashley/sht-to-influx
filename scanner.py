29e759b81cd005efc0a82ecaf396c7c990479fd5
Jordan Ashley <jashley6@vols.utk.edu>
2025-06-11 15:50:08 -0700

    :sparkles: Add UUID-finder

---
+++ scanner.py
@@ -0,0 +1,29 @@
+import asyncio
+from bleak import BleakClient
+
+async def main(a):
+    async with BleakClient(a, timeout=30.0) as client:
+        services = await client.get_services()
+        for service in services:
+            print(f"Service: {service.uuid} | {service.description}")
+            for char in service.characteristics:
+                print(f"  Characteristic: {char.uuid} | {char.description}")
+
+asyncio.run(main())
+
+if __name__ == "__main__":
+    # parse name, address arguments
+    parser = argparse.ArgumentParser(
+        description="Connect to SHT via MAC address"
+    )
+    parser.add_argument(
+        '--address', 
+        type=str, 
+        required=True, 
+        help='MAC address of the BLE device (e.g., AA:BB:CC:DD:EE:FF)'
+    )
+    args    = parser.parse_args()
+    address = args.address
+    
+    # do the things
+    asyncio.run(main(address))
